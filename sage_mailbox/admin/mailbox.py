3dd775c2b5fca3882163220e6c1dda7deba68fc1
MohmdFo <mohammad.fotouhi80@gmail.com>
2024-08-26 13:31:36 +0330

    feat(MailboxAdmin): Enhance redirection handling after saving models
    - Added handling for "_save" action in `response_add` and `response_change` methods to improve redirection behavior.
    - Modified `response_add` to redirect to the changelist page after saving a new model, if the "_save" button is pressed.
    - Modified `response_change` to redirect to the changelist page after saving changes to an existing model, if the "_save" button is pressed.
    - This change ensures a smoother user experience by preventing issues related to double flash messages in the Django admin interface.

--- sage_mailbox/admin/mailbox.py
+++ sage_mailbox/admin/mailbox.py
@@ -9,6 +9,7 @@ from django.http import HttpRequest, HttpResponseRedirect
 from django.http.response import HttpResponse
 from django.urls import path, reverse
 from django.utils.translation import gettext_lazy as _
+
 from sage_imap.exceptions import (
     IMAPFolderExistsError,
     IMAPFolderNotFoundError,
@@ -19,6 +20,7 @@ from sage_imap.services import IMAPClient, IMAPFolderService
 from sage_mailbox.admin.actions import delete_selected
 from sage_mailbox.models.mailbox import Mailbox, StandardMailboxNames
 
+# IMAP configuration
 imap_host = getattr(settings, 'IMAP_SERVER_DOMAIN', None)
 imap_username = getattr(settings, 'IMAP_SERVER_USER', None)
 imap_password = getattr(settings, 'IMAP_SERVER_PASSWORD', None)
@@ -27,8 +29,9 @@ imap_password = getattr(settings, 'IMAP_SERVER_PASSWORD', None)
 @admin.register(Mailbox)
 class MailboxAdmin(admin.ModelAdmin):
     change_list_template = "admin/mailbox/change_list.html"
-
-    list_display = ("name", "slug", "folder_type", "created_at", "modified_at")
+    list_display = (
+        "name", "slug", "folder_type", "created_at", "modified_at"
+    )
     search_fields = ("name",)
     ordering = ("name",)
     list_per_page = 25
@@ -75,7 +78,8 @@ class MailboxAdmin(admin.ModelAdmin):
                             raise ValidationError(str(e))
                     super().save_model(request, obj, form, change)
                 messages.success(
-                    request, f'The Mailbox "{new_name}" was added successfully.'
+                    request,
+                    f'The Mailbox "{new_name}" was added successfully.'
                 )
         except ValidationError as e:
             messages.error(request, f"Error: {e.message}")
@@ -101,11 +105,11 @@ class MailboxAdmin(admin.ModelAdmin):
                     try:
                         folder_service.delete_folder(obj.name)
                     except IMAPFolderNotFoundError:
-                        # If folder is not found on IMAP, delete only in Django
                         super().delete_model(request, obj)
                         messages.warning(
                             request,
-                            f'The Mailbox "{obj.name}" was deleted from admin, but it did not exist on the IMAP server.',
+                            f'The Mailbox "{obj.name}" was deleted from admin, '
+                            'but it did not exist on the IMAP server.'
                         )
                     except IMAPFolderOperationError as e:
                         raise ValidationError(str(e))
@@ -113,7 +117,8 @@ class MailboxAdmin(admin.ModelAdmin):
                         super().delete_model(request, obj)
                         messages.success(
                             request,
-                            f'The Mailbox "{obj.name}" was deleted successfully from both admin and IMAP server.',
+                            f'The Mailbox "{obj.name}" was deleted successfully '
+                            'from both admin and IMAP server.'
                         )
         except ValidationError as e:
             messages.error(request, f"Error: {e.message}")
@@ -129,11 +134,11 @@ class MailboxAdmin(admin.ModelAdmin):
                         try:
                             folder_service.delete_folder(obj.name)
                         except IMAPFolderNotFoundError:
-                            # If folder is not found on IMAP, delete only in Django
                             super().delete_model(request, obj)
                             messages.warning(
                                 request,
-                                f'The mailbox "{obj.name}" was deleted from admin, but it did not exist on the IMAP server.',
+                                f'The mailbox "{obj.name}" was deleted from admin, '
+                                'but it did not exist on the IMAP server.'
                             )
                         except IMAPFolderOperationError as e:
                             raise ValidationError(str(e))
@@ -142,7 +147,8 @@ class MailboxAdmin(admin.ModelAdmin):
                     super().delete_queryset(request, queryset)
                 messages.success(
                     request,
-                    "The selected Mailboxes were deleted successfully from both admin and IMAP server.",
+                    "The selected Mailboxes were deleted successfully "
+                    "from both admin and IMAP server."
                 )
         except ValidationError as e:
             messages.error(request, f"Error: {e.message}")
@@ -150,22 +156,43 @@ class MailboxAdmin(admin.ModelAdmin):
             messages.error(request, f"Unexpected error: {str(e)}")
 
     def response_add(self, request, obj, post_url_continue=None):
+        """
+        Handles the HTTP response after adding a new object in the Django admin.
+
+        This method is overridden to prevent the double flash message issue
+        that can occur in Django's admin interface.
+        """
         if "_continue" in request.POST:
             return super().response_add(request, obj, post_url_continue)
         elif "_addanother" in request.POST:
             return HttpResponseRedirect(request.path)
-        else:
-            return HttpResponseRedirect(self.get_success_url(request, obj))
+        elif "_save" in request.POST:
+            post_url = reverse(
+                "admin:%s_%s_changelist" % (self.opts.app_label, self.opts.model_name),
+                current_app=self.admin_site.name,
+            )
+            return HttpResponseRedirect(post_url)
+        return HttpResponseRedirect(self.get_success_url(request, obj))
 
     def response_change(self, request, obj):
+        """
+        Handles the HTTP response after changing an existing object in the Django admin.
+
+        Similar to `response_add`, this method prevents the issue of double flash messages.
+        """
         if "_continue" in request.POST:
             return super().response_change(request, obj)
         elif "_saveasnew" in request.POST:
             return super().response_add(request, obj)
         elif "_addanother" in request.POST:
             return HttpResponseRedirect(request.path)
-        else:
-            return HttpResponseRedirect(self.get_success_url(request, obj))
+        elif "_save" in request.POST:
+            post_url = reverse(
+                "admin:%s_%s_changelist" % (self.opts.app_label, self.opts.model_name),
+                current_app=self.admin_site.name,
+            )
+            return HttpResponseRedirect(post_url)
+        return HttpResponseRedirect(self.get_success_url(request, obj))
 
     def response_delete(
         self, request: HttpRequest, obj_display: str, obj_id: int
@@ -180,13 +207,17 @@ class MailboxAdmin(admin.ModelAdmin):
         return request.META.get("HTTP_REFERER", "/admin/")
 
     def sync_folders(self, request):
+        """
+        Synchronizes the mailboxes between the IMAP server and the Django admin.
+        """
         try:
             with IMAPClient(imap_host, imap_username, imap_password) as client:
                 folder_service = IMAPFolderService(client)
                 folders = folder_service.list_folders()
                 for folder_name in folders:
                     Mailbox.objects.update_or_create(
-                        name=folder_name, defaults={"slug": folder_name.lower()}
+                        name=folder_name,
+                        defaults={"slug": folder_name.lower()}
                     )
                 messages.success(request, "Mailboxes synchronized successfully.")
         except Exception as e:
@@ -194,6 +225,6 @@ class MailboxAdmin(admin.ModelAdmin):
         return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/admin/"))
 
     def log_deletion(
-        self, request: HttpRequest, object: Any, object_repr: str
+        self, request: HttpRequest, obj: Any, object_repr: str
     ) -> LogEntry:
-        return super().log_deletion(request, object, object_repr)
+        return super().log_deletion(request, obj, object_repr)
